e1acb52d1c0fa886b490ad2c799cedc9f3eeab01
Keita-lee <keita@barefootbytes.com>
2025-03-18 11:42:27 +0200

    set up firebase

--- my_jbay/lib/main.dart
+++ my_jbay/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:my_jbay/commanUi/DifferentBottomNavbars/nav_with_now_on.dart';
 import 'package:my_jbay/constants/navbar_controller.dart';
@@ -5,7 +7,21 @@ import 'package:provider/provider.dart';
 
 import 'package:my_jbay/MainLandingPage/landingPage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyCQsH25T5ri_msQmlMyaNk8QHoobgdXD0k",
+            authDomain: "my-jbay-app.firebaseapp.com",
+            projectId: "my-jbay-app",
+            storageBucket: "my-jbay-app.firebasestorage.app",
+            messagingSenderId: "475469224311",
+            appId: "1:475469224311:web:d40cfb541d230690382972"));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(
     MultiProvider(
       providers: [
